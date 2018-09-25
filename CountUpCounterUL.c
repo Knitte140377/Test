108df86badc56648593c20f32021ecbe518102ab
Markus <43406203+Knitte140377@users.noreply.github.com>
2018-09-25 13:44:09 +0200

    Create CountUpCounterUL.c

---
+++ CountUpCounterUL.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int countUpCounter =0;
+    do{
+        printf("%d\n" , countUpCounter);
+    } while(countUpCounter++ < 100);
+        
+    printf("TouchDown\n");
+    
+    return 0;
+ }
