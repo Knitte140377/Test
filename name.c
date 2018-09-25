d7ce3da5c403fc96537a11848ebdfbf52af5ee52
Markus <43406203+Knitte140377@users.noreply.github.com>
2018-09-25 13:45:22 +0200

    Create name.c

---
+++ name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char * a_vorname = "Markus";
+    char * a_nachname = "Brückler";
+    printf("Hello  %s %s\n", a_vorname, a_nachname);
+    
+    return 0;
+}
